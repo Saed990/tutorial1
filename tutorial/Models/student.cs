78d173e9d60a95f2d46cd261614036154927c09a
saedt <saedt@DESKTOP-ANPDBMH>
2019-04-04 23:05:12 +0100

    add list

---
+++ tutorial/Models/student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tutorial.Models
+{
+    public class student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public standard standard { get; set; }
+    }
+
+    public class standard
+    {
+        public int standatdID { get; set; }
+        public string standartNAme { get; set; }
+    }
+}
