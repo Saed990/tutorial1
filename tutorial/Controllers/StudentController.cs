f45ccd50f96d5c0d25ba9e3b1f2a1167d8b7115d
saedt <saedt@DESKTOP-ANPDBMH>
2019-04-07 22:42:27 +1200

    make static data

--- tutorial/Controllers/StudentController.cs
+++ tutorial/Controllers/StudentController.cs
@@ -7,12 +7,10 @@ using tutorial.Models;
 
 namespace tutorial.Controllers
 {
-    public class StudentController : Controller
+
+    public class data
     {
-        // GET: Student
-        public ActionResult Index()
-        {
-            var studentlist = new List<student> {
+       public static List<student> studentlist = new List<student> {
                 new student{ ID=1, name="Ali", age=34},
                 new student{ ID=2, name="Khalid", age=11},
                 new student{ ID=3, name="Mohamad", age=34},
@@ -20,12 +18,20 @@ namespace tutorial.Controllers
                 new student{ ID=5, name="Ali", age=22},
                 new student{ ID=6, name="Anas", age=40},
             };
+    }
 
-            return View(studentlist);
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+
+            return View(tutorial.Controllers.data.studentlist);
         }
 
         public ActionResult edit(int id) {
-            //do something
+            
             return View();
         }
 
@@ -37,24 +43,6 @@ namespace tutorial.Controllers
 
             //update database
             return RedirectToAction("Index");
-        }
-
-        [ActionName("Find")]
-        public ActionResult GetById(int id)
-        {
-            return View();
-        }
-
-        [NonAction]
-        public ActionResult GetByID(int id)
-        {
-            return View();
-        }
-
-        [AcceptVerbs(HttpVerbs.Post | HttpVerbs.Get)]
-        public ActionResult GetandPost()
-        {
-            return View();
         }
 
 
